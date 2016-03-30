8b4cb7fb52ef0038478b878396d9a6140d5a95ea
liuyl <liuyl@kaolafm.com>
2016-03-30 20:33:25 +0800

    1.add listener;

--- app/src/main/java/com/novelot/demo_tablayout/MainActivity.java
+++ app/src/main/java/com/novelot/demo_tablayout/MainActivity.java
@@ -5,18 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTab = (TextView) findViewById(R.id.tv);
         TabLayout mTabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         int i = 0;
         for (int count = 4; i < count; ++i) {
             mTabLayout.addTab(mTabLayout.newTab().setText("Title-" + i));
         }
+
+        mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                tvTab.setText(tab.getPosition() + " selected");
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tvTab.setText(tab.getPosition() + " unselected");
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Do nothing
+                tvTab.setText(tab.getPosition() + " reselected");
+            }
+        });
     }
 
 }
